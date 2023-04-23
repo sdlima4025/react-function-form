03cd27b6ce9fddfd5a1dfe1081be266cb537a4fc
Sérgio Lima <sergiolimafg3@gmail.com>
2023-04-23 12:03:47 -0300

    refatorado código formulario e criado formulario de login.

---
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { TextField, Button } from "@mui/material";
+
+function DadosUsuario() {
+  return (
+    <form>
+      <TextField
+        id="email"
+        label="email"
+        type="email"
+        margin="normal"
+        autoFocus
+        fullWidth
+      />
+      <TextField
+        id="senha"
+        label="senha"
+        type="password"
+        margin="normal"
+        autoFocus
+        fullWidth
+      />
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
+
+export default DadosUsuario;
